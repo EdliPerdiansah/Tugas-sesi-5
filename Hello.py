0c8b617a4e17fd9aa9efe1bda68ad2af55287199
EdliPerdiansah <149916845+EdliPerdiansah@users.noreply.github.com>
2024-03-24 12:20:23 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,6 @@
+nama = input("Masukan nama Anda : ")
+mata_kuliah = input("Masukan nama kuliah yang anda pelajari: ")
+
+print("Hallo Selamat Datang", 
+      nama, ", Selamat Belajar",
+      mata_kuliah, "!")
---
+++ kalkulator.py
@@ -0,0 +1,6 @@
+celcius = input ("masukan celcius :")
+format = 9 * int(celcius) / 5 + 32
+
+print (celcius, "celcius adalah")
+print (int(format), "fahrenheit")
+    
